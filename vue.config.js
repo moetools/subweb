186ef035e1efd27bb97deecca3fcc5235108a276
Casper <hicasper@users.noreply.github.com>
2024-12-10 21:21:44 +0800

    feat: support subdir

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 module.exports = defineConfig({
-  // publicPath: '/',
+  publicPath: process.env.BASE_URL || '/',
   lintOnSave: false,
   transpileDependencies: true,
   productionSourceMap: false,
