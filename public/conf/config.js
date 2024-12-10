ed49c80992fe33c76af7eff3574c4b5459868ca0
Casper <hicasper@users.noreply.github.com>
2024-12-10 22:18:03 +0800

    deploy: custom configuration

--- public/conf/config.js
+++ public/conf/config.js
@@ -4,8 +4,24 @@ window.config = {
   // API 地址
   apiUrls: [
     {
+      value: 'https://subapi.fly.dev/sub?',
+      text: '默认'
+    },
+    {
+      value: 'https://sub.xeton.dev/sub?',
+      text: '第三方 - YToo [sub.xeton.dev]'
+    },
+    {
+      value: 'https://api.dler.io/sub?',
+      text: '第三方 - Dler [api.dler.io]'
+    },
+    {
+      value: 'https://sub.d1.mk/sub?',
+      text: '第三方 - 肥羊 [sub.d1.mk] [支持VLESS REALITY HY2]'
+    },
+    {
       value: 'http://127.0.0.1:25500',
-      text: '本地'
+      text: '本地 [127.0.0.1:25500]'
     }
   ],
   // 短域名服务地址
@@ -21,12 +37,16 @@ window.config = {
   // 远程配置地址,可以自行按照格式添加。
   remoteConfigOptions: [
     {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini',
-      text: 'ACL4SSR Online',
+      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
+      text: '基础规则（推荐）',
     },
     {
-      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini',
-      text: 'ACL4SSR Online Full',
+      value: 'https://raw.githubusercontent.com/MoeTools/static/master/config/clash-config.ini',
+      text: '进阶规则（详细）',
+    },
+    {
+      value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_BackCN.ini',
+      text: '回国规则',
     },
   ],
 };
